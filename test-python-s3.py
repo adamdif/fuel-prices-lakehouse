5f68ceb942b35f9f94e8d58d026dbc90a08514e7
Adam <adam.elmoutaoukil@ensiie.eu>
2025-12-02 11:49:34 +0100

    Add test of access to AWS S3 bucket (working)

--- test-python-s3.py
+++ test-python-s3.py
@@ -2,6 +2,23 @@ import os
 import boto3
 from dotenv import load_dotenv
 
+import pandas as pd
+
+##########
+"""
+The goal of this code is to :
+1) load the csv
+2) adds technical desired columns
+3) calculates record_hash
+4) normalizes types if necessary
+5) writes local parquet
+6) pushes it to s3 towards bronze/
+"""
+##########
+
+csv_headerline = "id;latitude;longitude;Code postal;pop;Adresse;Ville;services;prix;rupture;horaires;geom;Prix Gazole mis à jour le;Prix Gazole;Prix SP95 mis à jour le;Prix SP95;Prix E85 mis à jour le;Prix E85;Prix GPLc mis à jour le;Prix GPLc;Prix E10 mis à jour le;Prix E10;Prix SP98 mis à jour le;Prix SP98;Début rupture e10 (si temporaire);Type rupture e10;Début rupture sp98 (si temporaire);Type rupture sp98;Début rupture sp95 (si temporaire);Type rupture sp95;Début rupture e85 (si temporaire);Type rupture e85;Début rupture GPLc (si temporaire);Type rupture GPLc;Début rupture gazole (si temporaire);Type rupture gazole;Carburants disponibles;Carburants indisponibles;Carburants en rupture temporaire;Carburants en rupture definitive;Automate 24-24 (oui/non);Services proposés;Département;code_departement;Région;code_region;horaires détaillés"
+
+
 load_dotenv()
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -21,6 +38,6 @@ if "Contents" in response:
     for obj in response["Contents"]:
         print(obj["Key"])
 else:
-    print("Le bucket est vide ou problème de permissions.")
+    print("Bucket is empty or permissions issue")
 
 print(S3_BUCKET)
